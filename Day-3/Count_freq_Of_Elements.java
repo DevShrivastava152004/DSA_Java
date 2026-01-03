6f727ca66ec6a7c9c27e95061260e1ebf695bd82
Dev Shrivastava <devshrivastava621@gmail.com>
2026-01-03 23:30:55 +0530

    hashmap practise

---
+++ Day-3/Count_freq_Of_Elements.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class Count_freq_Of_Elements
+ {
+    public static void main(String[] args)
+    {
+        int arr[] = new int[]{15,16,27,27,28,15};
+        int n = arr.length;
+        
+        countFreq(arr, n);
+    }
+    static void countFreq(int arr[], int n)
+    {
+        HashMap<Integer, Integer> result = new HashMap<>();
+
+        for(int i=0; i<n ;i ++)
+        {
+            if(result.containsKey(arr[i])){
+                result.put(arr[i], result.get(arr[i])+1);
+            }
+            else{
+                result.put(arr[i], 1);
+            }
+        }
+        for(Map.Entry<Integer, Integer> i: result.entrySet()){
+            System.out.println(i.getKey() + " " + i.getValue());
+        }
+    }
+ }
